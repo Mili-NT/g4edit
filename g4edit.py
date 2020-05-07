81a5ccc2fa296394b06550959f21b341c73a5c09
Mili <52385020+Mili-NT@users.noreply.github.com>
2020-05-06 23:37:22 -0400

    remove comments

--- g4edit.py
+++ g4edit.py
@@ -1,22 +1,6 @@
 #!/usr/bin/python3
 import classes
 import sys
-"""
-LITTLE ENDIAN
-In Platinum:
----------------------------------------------------------------
-1. the first small block starts at 0x00000 and ends at 0x0CF2B
-2. the first big block starts at 0x0CF2C and ends at 0x1F10F. 
-3. The second pair of blocks are at the same address plus 0x40000 for all the three games.
----------------------------------------------------------------
-One block pair is always a backup of the other block pair. 
-
-
-Name -> DUMB : AAAAAAA (max 7 char)
-Rival Name -> FAGGOT : BBBBBBB
-TID -> 29156 : 48611
-SID -> 30151 : 45433
-"""
 # IMPORTANT FUNCTIONS
 def load_file(fp):
     with open(fp, "rb") as f:
